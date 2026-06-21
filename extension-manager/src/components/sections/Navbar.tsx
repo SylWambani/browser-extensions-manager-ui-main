142eaf19b75d00e4bfc0a6310b226d2696b43af0
SYLVIA WAMBANI <wambanisylvia2@gmail.com>
2026-06-21 15:54:17 +0300

    Toggle between light and dark mode

--- extension-manager/src/components/sections/Navbar.tsx
+++ extension-manager/src/components/sections/Navbar.tsx
@@ -1,13 +1,28 @@
 import { Box, HStack, Image } from "@chakra-ui/react";
 import logo from "../../assets/images/logo.svg";
-import sun from "../../assets/images/icon-sun.svg"
+import sun from "../../assets/images/icon-sun.svg";
+import moon from "../../assets/images/icon-moon.svg";
+import { useColorMode } from "../ui/color-mode";
 
 const Navbar = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
   return (
     <Box>
       <HStack>
-              <Image src={logo} alt="Logo" />
-              <Image src={sun} alt="Light Mode" />
+        <Image src={logo} alt="Logo" />
+        <Image
+          src={colorMode === "light" ? moon : sun}
+          alt={
+            colorMode === "light"
+              ? "Light mode - show moon"
+              : "Dark mode - show sun"
+          }
+          onClick={toggleColorMode}
+          cursor="pointer"
+          role="button"
+          aria-label="Toggle color mode"
+        />
       </HStack>
     </Box>
   );
