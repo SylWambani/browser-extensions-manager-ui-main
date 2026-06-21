22fe5051895b9a5ce5ee92bbbc60b1f7e2a7438a
SYLVIA WAMBANI <wambanisylvia2@gmail.com>
2026-06-21 16:43:01 +0300

    Make the status switch interactive

--- extension-manager/src/components/sections/ExtensionCards.tsx
+++ extension-manager/src/components/sections/ExtensionCards.tsx
@@ -1,41 +1,62 @@
-import { Card, SimpleGrid } from '@chakra-ui/react'
-import { useEffect, useState } from 'react';
-import { extensions } from '../data/extensionData';
+import { Card, SimpleGrid, Switch } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { extensions } from "../data/extensionData";
 
 const ExtensionCards = () => {
-    const [images, setImages] = useState<string[]>([]);
+  const [images, setImages] = useState<string[]>([]);
+  const [extensionSwitch, setExtensionSwitch] = useState(extensions);
 
-    useEffect(() => {
-        const imageModules = import.meta.glob<{default:string}>('../../assets/images/*.{png, jpg, jpeg, svg}');
+  useEffect(() => {
+    const imageModules = import.meta.glob<{ default: string }>(
+      "../../assets/images/*.{png, jpg, jpeg, svg}",
+    );
 
-        console.log(Object.keys(imageModules));
+    console.log(Object.keys(imageModules));
 
-        const loadImages = async () => {
-            const imageUrls = await Promise.all(
-                Object.values(imageModules).map(async (importImage) => {
-                    const mod = await importImage();
-                    return mod.default;
-                })
-            );
-            console.log(imageUrls);
-            setImages(imageUrls);
-        };
-        loadImages();
-    }, []);
-    
+    const loadImages = async () => {
+      const imageUrls = await Promise.all(
+        Object.values(imageModules).map(async (importImage) => {
+          const mod = await importImage();
+          return mod.default;
+        }),
+      );
+      console.log(imageUrls);
+      setImages(imageUrls);
+    };
+    loadImages();
+  }, []);
+
+  const handleToggle = (id: number) => {
+    setExtensionSwitch((prev) =>
+      prev.map((ext) =>
+        ext.id === id ? { ...ext, active: !ext.active } : ext,
+      ),
+    );
+  };
+
   return (
-      <SimpleGrid columns={{base:1, md:2, lg:4}} gap={2}>
-          {extensions.map((extension) => (
-              <Card.Root key={extension.id}>
-                  <Card.Body>
-                      <img src={extension.logo} alt={extension.name} width='20%' />
-                      <Card.Title>{extension.name}</Card.Title>
-                      <Card.Description>{extension.description}</Card.Description>
-                  </Card.Body>
-              </Card.Root>
-          ))}
-      </SimpleGrid>
-  )
-}
+    <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} gap={2}>
+      {extensionSwitch.map((extension) => (
+        <Card.Root key={extension.id}>
+          <Card.Body>
+            <img src={extension.logo} alt={extension.name} width="20%" />
+            <Card.Title>{extension.name}</Card.Title>
+            <Card.Description>{extension.description}</Card.Description>
+            <Switch.Root
+              checked={extension.active}
+              onCheckedChange={() => handleToggle(extension.id)}
+            >
+              <Switch.HiddenInput />
+              <Switch.Control>
+                <Switch.Thumb />
+              </Switch.Control>
+              <Switch.Label />
+            </Switch.Root>
+          </Card.Body>
+        </Card.Root>
+      ))}
+    </SimpleGrid>
+  );
+};
 
-export default ExtensionCards
+export default ExtensionCards;
